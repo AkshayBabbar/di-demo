81016cfbc1f24005e195f03425f7d6d232038c00
akshaybabbar12@gmail.com <akshaybabbar12@gmail.com>
2019-07-08 11:17:29 +0530

    controller added here.....

--- src/main/java/akshay/springframework/didemo/DiDemoApplication.java
+++ src/main/java/akshay/springframework/didemo/DiDemoApplication.java
@@ -1,13 +1,19 @@
 package akshay.springframework.didemo;
 
+import akshay.springframework.didemo.controller.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiDemoApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+        MyController Controller = (MyController) ctx.getBean("myController");
+
+        Controller.hello();
     }
 
 }
--- src/main/java/akshay/springframework/didemo/controller/MyController.java
+++ src/main/java/akshay/springframework/didemo/controller/MyController.java
@@ -1,4 +1,13 @@
 package akshay.springframework.didemo.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MyController {
+
+    public String hello() {
+        System.out.println("Hello");
+
+        return "Shit";
+    }
 }
